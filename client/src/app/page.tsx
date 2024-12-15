661b95c60dac086f80867f57c8a6667f5ac2a3f1
DevMhrn <debashismaharana7854@gmail.com>
2024-12-15 17:43:04 +0530

    Added the client side, and Cards
    Signed-off-by: DevMhrn <debashismaharana7854@gmail.com>

---
+++ client/src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeatureSection from "@/components/base/FeatureSection";
+
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col " suppressHydrationWarning >
+      
+
+      
+      <FeatureSection />
+
+      
+      
+    </div>
+
+  );
+}
+
