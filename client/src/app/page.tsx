7b32abdf1951bb123a33a5605fd1f0a5488514ac
DevMhrn <debashismaharana7854@gmail.com>
2024-12-15 18:00:35 +0530

    added the navbar, footer and herosection
    Signed-off-by: DevMhrn <debashismaharana7854@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,8 @@
 import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReview";
 
 import Image from "next/image";
 
@@ -7,13 +11,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col " suppressHydrationWarning >
+      {/* Header */}
       
+      <Navbar />
+      {/* Hero Section */}
+      <HeroSection />
 
-      
+      {/* Features Section */}
       <FeatureSection />
 
-      
-      
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
     </div>
 
   );
