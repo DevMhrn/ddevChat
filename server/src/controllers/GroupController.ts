75f779c34cb391aebbdb2d58d033c900b8343d0f
DevMhrn <debashismaharana7854@gmail.com>
2024-12-17 16:40:31 +0530

    Correct a small error
    Signed-off-by: DevMhrn <debashismaharana7854@gmail.com>

---
+++ server/src/controllers/GroupController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class ChatGroupController{
+
+    static async store(req:Request, res:Response){
+        
+    }
+
+}
---
+++ server/src/middleware/AuthMiddleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader === null || authHeader === undefined){
+        res.status(401).json({message:"Unauthorized"});
+        return;
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(err){
+            res.status(401).json({message:"Unauthorized"});
+            return;
+        }
+        
+    })
+
+}
